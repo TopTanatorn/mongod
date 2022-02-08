e0c086dbf6942dbdcc594fdb9bb9de39367a838c
TopTanatorn <s6230613017@phuket.psu.ac.th>
2022-02-08 13:54:54 +0700

    done

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ let database = require("./database");
 let BlockChain = require("./blockChain");
 let blockChain = new BlockChain();
 let hash = require("object-hash");
-
-
 database.onConnect(() => {   
+    
         blockChain.addNewBlock(null,"test2");  
+        
 });
--- src/test.js
+++ src/test.js
@@ -1,2 +1,17 @@
-const chalk = require('chalk');
-console.log(chalk.blue('Hello world!')); 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  a ="Top";
+  var myobj = [
+    { name: a, address: a},
+    
+  ];
+  dbo.collection("customers").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
